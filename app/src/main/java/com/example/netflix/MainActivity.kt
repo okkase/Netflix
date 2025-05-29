86de2ca961eb4f41060e7e57f763df71fa0cb7f6
okkase <felipe.costabeber.s@mail.com>
2025-05-29 12:51:46 -0300

    correção do viewBinding

--- app/src/main/java/com/example/netflix/MainActivity.kt
+++ app/src/main/java/com/example/netflix/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.netflix
-
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.netflix.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
     }
+
+
+
 }
+
+
